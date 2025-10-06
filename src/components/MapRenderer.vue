e32e955c45db0864aced391274d70d1c26007e83
Mauricio Alcantara da Luz <mauricio.luz22@edu.pucrs.br>
2025-10-05 21:33:25 -0300

    labels dos predios

--- src/components/MapRenderer.vue
+++ src/components/MapRenderer.vue
@@ -106,7 +106,7 @@ onMounted(async () => {
     try {
       const res = await fetch("http://localhost:8080/map");  // TODO: Mudar o endpoint para o correto
       const data = await res.json(); // Pega o corpo da resposta e transforma em JSON
-      // console.log("Dados do endpoint:", data);
+      console.log("Dados do endpoint:", data);
       // setStands(data.stands);  // Atualiza o estado com os dados recebidos da API
       // setRoads(data.roads);  // Atualiza o estado com os dados recebidos da API
       loadGround(data.streets)
@@ -154,31 +154,64 @@ function loadModels(data: Building[]) {
       model.position.set(coordinate.x, 0.1, coordinate.y)
       scene.add(model)
 
-      // Labels!!
-      // const box = new THREE.Box3().setFromObject(model)
-      // const size = new THREE.Vector3()
-      // const center = new THREE.Vector3()
-      // box.getSize(size)
-      // box.getCenter(center)
-      
-      
-      // let shape = font.generateShapes("91A", 3)
-	    // let geometryS = new THREE.ShapeGeometry(shape)
-      // geometryS.computeBoundingBox()
+      if (name === 'tecnopuc') {
+        return
+      }
 
-      // const textBox = new THREE.Box3().setFromObject(new THREE.Mesh(geometryS))
-      // const textBoxSize = new THREE.Vector3()
-      // const textBoxCenter = new THREE.Vector3()
-      // textBox.getSize(textBoxSize)
-      // textBox.getCenter(textBoxCenter)
+      // Labels!!
+      const box = new THREE.Box3().setFromObject(model)
+      const size = new THREE.Vector3()
+      const center = new THREE.Vector3()
+      box.getSize(size)
+      box.getCenter(center)
       
+      let shape = font.generateShapes(name, name.length < 5 ? 3.125 : 2.625)
+	    let geometryS = new THREE.ShapeGeometry(shape)
+      geometryS.computeBoundingBox()
+
+      // Adaptado daqui: https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_text_shapes.html#L76
+      const holeShapes = [];
+			for ( let i = 0; i < shape.length; i ++ ) {
+				const sh = shape[i];
+				if ( sh.holes && sh.holes.length > 0 ) {
+					for ( let j = 0; j < sh.holes.length; j ++ ) {
+						const hole = sh.holes[j];
+						holeShapes.push(hole);
+					}
+				}
+      }
+
+      shape.push( ...holeShapes );
+			const lineText = new THREE.Object3D();
+			for ( let i = 0; i < shape.length; i ++ ) {
+				const sh = shape[ i ];
+				const points = sh.getPoints();
+				const geometry = new THREE.BufferGeometry().setFromPoints(points);
+				const lineMesh = new THREE.Line( geometry, new THREE.LineBasicMaterial({ color:0x000000 }));
+				lineText.add(lineMesh);
+			}
+      // fim da adaptacao
+
+      const textBox = new THREE.Box3().setFromObject(new THREE.Mesh(geometryS))
+      const textBoxSize = new THREE.Vector3()
+      const textBoxCenter = new THREE.Vector3()
+      textBox.getSize(textBoxSize)
+      textBox.getCenter(textBoxCenter)      
       
-      // const labelPos = [(center.x - size.x/2) + (size.x - textBoxSize.x) / 2, center.y + size.y / 2 + 0.2, (center.z - size.z/2) + (size.z - textBoxSize.z) / 2]
-      // const textMesh = new THREE.Mesh(geometryS, new THREE.MeshBasicMaterial({ color: 0xffffff }))
-      // // textMesh.scale.set(.5, .5, .5)
-      // textMesh.rotation.set(-Math.PI / 2, 0, -Math.PI / 2)
-      // textMesh.position.set(labelPos[0], labelPos[1], labelPos[2])
-      // scene.add(textMesh)
+      const corner = [center.x - size.x/2, center.y + size.y/2, center.z - size.z/2]
+      const labelPos = [(size.x - textBoxSize.y) / 2, 0, (size.z - textBoxSize.x) / 2]
+      // labelPos[0] += 1.5
+      // labelPos[2] -= 3
+      const textMesh = new THREE.Mesh(geometryS, new THREE.MeshBasicMaterial({ color: 0xffffff }))
+      textMesh.rotation.set(-Math.PI / 2, 0, -Math.PI / 2)
+      textMesh.position.set(labelPos[0] + corner[0], labelPos[1] + corner[1] + 0.1, labelPos[2] + corner[2])
+      
+      // const contourMesh = new THREE.Mesh(contourGeo, new THREE.MeshBasicMaterial({ color: 0x000000 }))
+      // contou.scale.set(.5, .5, .5)
+      // contourMesh.position.x -= .5
+      textMesh.add(lineText)
+      
+      scene.add(textMesh)
     })
   })
 }
